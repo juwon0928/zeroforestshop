5e5c25f81e954693f67595e26ee853a5062ea268
ParkSooHyun1747 <qkrtngus9184Anaver.com>
2021-11-24 16:36:43 +0900

    zeroforest update

--- zshop/settings/base.py
+++ zshop/settings/base.py
@@ -17,7 +17,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+# ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 INSTALLED_APPS = [
     'baton',
--- zshop/settings/development.py
+++ zshop/settings/development.py
@@ -1,7 +1,7 @@
 from .base import *
 
-DEBUG = True
-ALLOWED_HOSTS = ['127.0.0.1']
+DEBUG = False
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 INSTALLED_APPS += [
     'debug_toolbar'
@@ -37,7 +37,7 @@ DEBUG_TOOLBAR_CONFIG = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 DATABASES['default'].update(dj_database_url.config(conn_max_age=500))
